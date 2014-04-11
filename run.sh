d1855db542a62234bd63ba3bc68598b0471da41c
Gabriel Begun <begunbegun@gmail.com>
2014-04-10 23:07:00 -0400

    run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #run stuff
+rm /var/lock/LCK..*
 echo BB-UART1 > /sys/devices/bone_capemgr.8/slots
 echo BB-UART2 > /sys/devices/bone_capemgr.8/slots
 echo BB-UART4> /sys/devices/bone_capemgr.8/slots
@@ -8,4 +9,4 @@ stty -F /dev/ttyO2 9600 raw
 stty -F /dev/ttyO4 9600 raw
 stty -F /dev/ttyO5 9600 raw
 cd bin
-java -cp .:/usr/share/java/rxtx.jar -Djava.library.path=/usr/lib/jni/ -Dgnu.io.rxtx.SerialPorts=/dev/ttyO4 FireScout.FireScout 2>> "run_error.txt"
+java -cp .:/usr/share/java/rxtx.jar -Djava.library.path=/usr/lib/jni/ -Dgnu.io.rxtx.SerialPorts=/dev/ttyO1:/dev/ttyO2:/dev/ttyO4:/dev/ttyO5 FireScout.FireScout 2>> "run_error.txt"
