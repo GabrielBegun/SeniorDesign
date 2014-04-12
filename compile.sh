fad829159dbadcad3296c97fafdbb49d41a8331a
MikhailAndreev <mikha.andreev@gmail.com>
2014-04-11 23:06:44 -0400

    Updates to the compile/run/update scripts

--- compile.sh
+++ compile.sh
@@ -1,13 +1,14 @@
 # Compile the quadcopter source
 COMM_PACKAGE="src/communication/IRCamera.java src/communication/Pilot.java src/communication/PilotController.java src/communication/XbeeInterface.java"
-UTIL_PACKAGE="src/util/UartDriver.java"
+UTIL_PACKAGE="src/util/UartDriver.java src/util/Param.java"
 LOGGER_PACKAGE="src/Logger/Logger.java"
 FIRESCOUT_PACKAGE="src/FireScout/FireScout.java"
-DEFAULTS_PACKAGE="src/defaults/Param.java"
 SENSOR_PACKAGE="src/sensor/SensorManager/SensorManager.java src/sensor/SensorManager/LaserSensorInterface.java src/sensor/SensorManager/SonarGPIOSensorInterface.java src/sensor/SensorManager/SonarAnalogSensorInterface.java"
 RXTX_JAR="lib/RXTXcomm.jar"
 OUTPUT_FOLDER="bin"
 ERROR_FILE="error.txt"
 
+javac -d $OUTPUT_FOLDER -classpath $RXTX_JAR $LOGGER_PACKAGE $COMM_PACKAGE $UTIL_PACKAGE $FIRESCOUT_PACKAGE $SENSOR_PACKAGE 2> $ERROR_FILE
 
-javac -d $OUTPUT_FOLDER -classpath $RXTX_JAR $LOGGER_PACKAGE $COMM_PACKAGE $UTIL_PACKAGE $FIRESCOUT_PACKAGE $DEFAULTS_PACKAGE $SENSOR_PACKAGE 2> $ERROR_FILE
+gcc -o s_gpio src/sensor/SensorManager/sonar_gpio.c
+mv src/sensor/SensorManager/s_gpio bin/sensor/SensorManager/
--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #run stuff
 rm /var/lock/LCK..*
+echo cape-bone-iio > /sys/devices/bone_capemgr.8/slots 
 echo BB-UART1 > /sys/devices/bone_capemgr.8/slots
 echo BB-UART2 > /sys/devices/bone_capemgr.8/slots
-echo BB-UART4> /sys/devices/bone_capemgr.8/slots
+echo BB-UART4 > /sys/devices/bone_capemgr.8/slots
 echo BB-UART5 > /sys/devices/bone_capemgr.8/slots
 stty -F /dev/ttyO1 9600 raw
 stty -F /dev/ttyO2 9600 raw
 stty -F /dev/ttyO4 9600 raw
 stty -F /dev/ttyO5 9600 raw
+JAVA_HOME=/home/root/java/jdk1.7.0_51/
 cd bin
-java -cp .:/usr/share/java/rxtx.jar -Djava.library.path=/usr/lib/jni/ -Dgnu.io.rxtx.SerialPorts=/dev/ttyO1:/dev/ttyO2:/dev/ttyO4:/dev/ttyO5 FireScout.FireScout 2>> "run_error.txt"
+/home/root/java/jdk1.7.0_51/bin/java -cp .:/usr/share/java/rxtx.jar -Djava.library.path=/usr/lib/jni/ -Dgnu.io.rxtx.SerialPorts=/dev/ttyO1:/dev/ttyO2:/dev/ttyO4:/dev/ttyO5 FireScout.FireScout 2>> "run_error.txt"
