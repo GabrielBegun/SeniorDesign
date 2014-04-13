cafd1d4d33cd48b65727ba764926408e32839065
Gabriel Begun <begunbegun@gmail.com>
2014-04-13 02:31:59 -0400

    Params update

--- src/util/Param.java
+++ src/util/Param.java
@@ -4,11 +4,11 @@ public class Param {
 	// Throttle - Altitude 
 	public static final int throttleMax = 500; //very high, will change
 	public static final int throttleMin = 130;
-	public static final int throttleDeltaMax = 10;
+	public static final int throttleDeltaMax = 5;
 	public static final double throttleScale = 1;
-	public static final double kP_t = 0.1;
+	public static final double kP_t = 0.05;
 	public static final double kI_t = 0.02;
-	public static final double kD_t = 0.005;
+	public static final double kD_t = 0.01;
 
 	// Pitch - Laser Range Finder
 	public static final int pitchMax = 20; //very high, will change
