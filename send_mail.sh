c68afe77d330c00cbe9b91c40696c74bcfb2510d
MikhailAndreev <mikha.andreev@gmail.com>
2014-04-13 23:59:04 -0400

    send mail update

--- send_mail.sh
+++ send_mail.sh
@@ -6,6 +6,7 @@ if [ ! -f version ]; then
 fi
 VER=$(cat version)
 
+
 python mail.py $VER compilation_errors.txt
 python mail.py $VER compile_log.txt
 python mail.py $VER execution_error.txt
@@ -13,5 +14,14 @@ python mail.py $VER onRebootRun_log.txt
 python mail.py $VER run_log.txt
 python mail.py $VER update_log.txt
 
+cd /home/root/SeniorDesign/previousLogs
+NUM=$(ls -1 | wc -l)
+NAME=
+if [ "$NUM" -lt 10 ]; 	then
+	NAME="log0"$NUM".txt"
+else
+	NAME="log"$NUM".txt"
+echo $NAME
+	
 ((VER++))
 echo $VER > version
