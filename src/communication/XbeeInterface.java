71b5bef3cbd0dcb1e25ef4939bbaf135fb192927
Gabriel Begun <begunbegun@gmail.com>
2014-04-15 13:32:43 -0400

    Message update verifier

--- src/communication/XbeeInterface.java
+++ src/communication/XbeeInterface.java
@@ -47,7 +47,7 @@ public class XbeeInterface {
 		uartXbee.serialPort.addEventListener(new xBeeSerialPortEventListener()); // Throws
 		fireScout = FireScout.getInstance();
 		logger = Logger.getInstance();
-		this.write("XBeeInterface: Connection started, WE BUILT THIS CITY!");
+		this.write("XBeeInterface: Connection started, DO YOU WANT TO BUILD A SNOWMAN?"); // Change this message to check for updates
 	}
 	
 	public synchronized void write(String str) {
